de869629070174cbde5ebb831bd9fc337025e68d
Oleg Lykasov <gelo117@gmail.col>
2023-12-09 21:18:49 +0400

    - implemented exercise page logic

--- src/pages/exercise-page/exercise-page.tsx
+++ src/pages/exercise-page/exercise-page.tsx
@@ -1,21 +1,61 @@
-import React, {useState} from 'react';
+import React, {Fragment, useCallback, useState} from 'react';
 import s from './exercise-page.module.css';
 
 interface IProps {
     words: { [key: string]: string },
 }
 
-function ExercisePage({ words }: IProps) {
+const getRandomWord = (words: IProps['words']) => {
     const keys = Object.keys(words);
-    const [counter, setCounter] = useState(1);
-    const [currentWord, setNewWord] =
-        useState(keys[Math.round(Math.random() * keys.length)]);
+    return keys[Math.round(Math.random() * keys.length)];
+}
+
+function ExercisePage({ words }: IProps) {
+    const [counter, setCounter] = useState(0);
+    const [correctCounter, setCorrectCounter] = useState(0);
+    const [currentWord, setNewWord] = useState(getRandomWord(words));
+    const [isInCheckState, setCheckStateFlag] = useState(false);
+
+    const handleCheckBtnClick = useCallback(() => {
+        setCheckStateFlag(true);
+    }, []);
+
+    const handleBtnClick = useCallback((isCorrect: boolean) => {
+        setCounter(counter + 1);
+        if (isCorrect) {
+            setCorrectCounter(correctCounter + 1);
+        }
+        setNewWord(getRandomWord(words));
+        setCheckStateFlag(false);
+    }, [counter, words]);
 
     return <div className={s.container}>
-        <div className={s.counter}>{counter}</div>
-        Total: {keys.length}
+        <div className={s.counter}>
+            Words in pull: {Object.keys(words).length}
+            <br/>
+            Correctness: {counter === 0 ? '-' :
+            `${Math.round(correctCounter / counter * 10000) / 100}% (${correctCounter} / ${counter})`
+        }
+        </div>
+        <div>How to translate the next word:</div>
+        <span><b>{currentWord}</b>?</span>
         <br/>
-        {currentWord}
+        {isInCheckState ?
+            <div className={s.checkBlock}>
+                <div>This means:</div>
+                <div><b>{words[currentWord]}</b></div>
+                <br/>
+                <div className={s.checkTitle}>You were right?</div>
+                <div>
+                    <button onClick={() => handleBtnClick(true)}>Yes</button>
+                    &nbsp;&nbsp;
+                    <button onClick={() => handleBtnClick(false)}>Nope</button>
+                </div>
+            </div> :
+            <div className={s.checkBlock}>
+                <button onClick={handleCheckBtnClick}>Check</button>
+            </div>}
+
     </div>;
 }
 
